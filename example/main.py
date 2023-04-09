3cf39fa3aeb85f7425afdde26880db1fba25a688
Powei Lin <poweilin1994@gmail.com>
2023-04-09 17:49:08 -0400

    clean up

--- example/main.py
+++ example/main.py
@@ -4,14 +4,11 @@ from aprilgrid import Detector
 from glob import glob
 
 if __name__ == '__main__':
-    file_list = sorted(glob("dataset-calib-cam1_1024_16/mav0/cam0/data/*.png"))
-    # file_list = sorted(glob("dataset-calib-cam1_512_16/mav0/cam0/data/*.png"))
-    # file_list = sorted(glob("example/data/*.jpg"))
+    file_list = sorted(glob("example/data/*.jpg"))
     detector = Detector('t36h11')
     count = 0
     for i, file_name in enumerate(file_list):
         img = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
-        # img = cv2.resize(img, (952, 1264), None)
         img_color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
         detections = detector.detect(img)
         print(f"frame {i}, detect {len(detections)} tags")
@@ -26,13 +23,7 @@ if __name__ == '__main__':
                 cv2.circle(img_color, c, 3, (0, 255, 0))
 
 
-        # print(img.shape)
-        # print(im_max.shape)
         img_color = cv2.resize(img_color, None, None, 0.5, 0.5)
         cv2.imshow("im", img_color)
-        # cv2.imshow("im_max", im_max)
         cv2.waitKey(1)
     print(f"avg: {count/len(file_list):.3f} tags")
-    # 512 12.229
-    # 1024 32.718
-    # 7pro 33.000
