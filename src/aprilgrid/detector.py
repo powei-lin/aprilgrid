228aedfed12416bee08a21b958c19be8fbc8d8c5
Powei Lin <poweilin1994@gmail.com>
2023-04-12 21:03:17 -0400

    remove unused

--- src/aprilgrid/detector.py
+++ src/aprilgrid/detector.py
@@ -3,7 +3,6 @@ import numpy as np
 import cv2
 from typing import List
 from .tag_family import TAG_FAMILY_DICT
-from .qtp import ApriltagQuadThreshParams
 from .detection import Detection
 from .common import max_pool, random_color
 
@@ -15,6 +14,7 @@ class Detector:
     quad_sigma = 0.0
     refine_edges: bool = True
     decode_sharpening: float = 0.25
+    debug_level: int = 0
 
     def __post_init__(self):
         self.tag_family = TAG_FAMILY_DICT[self.tag_family_name]
@@ -52,22 +52,22 @@ class Detector:
 
     def apriltag_quad_thresh(self, im: np.ndarray):
         # step 1. threshold the image, creating the edge image.
-        h, w = im.shape[0], im.shape[1]
 
         im_copy = im.copy()
 
         threshim = self.threshold(im_copy)
-        # cv2.imshow("threshim", threshim)
-        # threshim = cv2.GaussianBlur(threshim, (3, 3), 1)
         (cnts, _) = cv2.findContours(threshim,
                                      cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
         # debug
-        # output = np.zeros((h, w, 3), dtype=np.uint8)
-        # for c in cnts:
-        #     cv2.drawContours(output, [c], -1, random_color(), 2)
-        #     cv2.imshow("debug", output)
-        #     cv2.waitKey(0)
+        if self.debug_level > 0:
+            h, w = im.shape[0], im.shape[1]
+            output = np.zeros((h, w, 3), dtype=np.uint8)
+            if self.debug_level == 1:
+                for c in cnts:
+                    cv2.drawContours(output, [c], -1, random_color(), 2)
+                    cv2.imshow("debug", output)
+                    cv2.waitKey(0)
 
         cnts = [c for c in cnts if (c.shape[0] >= 4)]
         quads = []  # array of quad including four peak points
@@ -77,9 +77,10 @@ class Detector:
                 hull = cv2.convexHull(c)
                 areahull = cv2.contourArea(hull)
                 # debug
-                # cv2.drawContours(output, [c], -1, random_color(), 2)
-                # cv2.imshow("debug", output)
-                # cv2.waitKey(0)
+                if self.debug_level == 2:
+                    cv2.drawContours(output, [c], -1, random_color(), 2)
+                    cv2.imshow("debug", output)
+                    cv2.waitKey(0)
                 if (area / areahull > 0.8):
                     # maximum_area_inscribed
                     quad = cv2.approxPolyDP(hull, 8, True)
@@ -88,9 +89,6 @@ class Detector:
                         if areaqued / areahull > 0.8 and areahull >= areaqued:
                             # Calculate the refined corner locations
                             quads.append(quad)
-        # cv2.drawContours(output, quads, -1, (0, 255, 0), 2)
-        # cv2.imshow("debug", output)
-        # cv2.waitKey(0)
         return quads
 
     def threshold(self, im: np.ndarray) -> np.ndarray:
@@ -100,13 +98,9 @@ class Detector:
         im_max = max_pool(im, tilesz, True)
         im_min = max_pool(im, tilesz, False)
 
-        # small_im = cv2.resize(im, None, None, 0.25, 0.25)
         kernel0 = np.ones((3, 3), dtype=np.uint8)
         im_max = cv2.dilate(im_max, kernel0)
-        # # im_max = cv2.resize(im_max, (w, h))
-        # kernel1 = np.ones((3, 3), dtype=np.uint8)
         im_min = cv2.erode(im_min, kernel0)
-        # im_min = cv2.resize(im_min, (w, h))
         im_min = np.repeat(np.repeat(im_min, tilesz, axis=1), tilesz, axis=0)
         im_max = np.repeat(np.repeat(im_max, tilesz, axis=1), tilesz, axis=0)
         
@@ -115,12 +109,10 @@ class Detector:
         im_max = np.pad(im_max, (0, edge), 'edge')[:h, :w]
 
         im_diff = im_max-im_min
-        # im_diff = cv2.resize(im_diff, (w, h))
-        # dd = np.where(diff < self.qtp.min_white_black_diff, 127, im_min)
         threshim = np.where(im_diff < self.qtp.min_white_black_diff, np.uint8(0),
                             np.where(im > (im_min + im_diff // 2), np.uint8(255), np.uint8(0)))
         
-        # hires can try dilate twice
+        # hi-res img can try dilate twice
         threshim = cv2.dilate(threshim, kernel0)
         return threshim
 
